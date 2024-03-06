b5e51e91e12c017a66547a04ca998c35dfb4e535
marllanesgarcia <sakiro1999@gmail.com>
2024-03-06 11:54:08 +0100

    modificando
    borrando codigo y añadiendo archivos para otros apartados

--- mar/ProyectoFinal/codigo.js
+++ mar/ProyectoFinal/codigo.js
@@ -25,33 +25,3 @@ function mostrarMenu() {
         }, 500);
     }
 }
-
-
-
-
-/*
-
-opcion 1: 
-
-function mostrarMenu(){
-
-    var posicionX = event.clientX - parent.offsetLeft;
-    var posicionY = event.clientY - parent.offsetTop;
-    var divPequeño = 3;
-    var radio = 50;
-    var distancia = (2*Math.PI)/divLogo;
-    
-    for (var i = 0;i < divPequeño; i++){
-        var divNuevo = document.createElement('div');
-        var textoNuevo = document.createElement('h5');
-        textoNuevo.textContent = 'tituloTituo';
-        divNuevo.style.top = posicionY+radio*Math.sin(i*distancia)+'px';
-        divNuevo.style.left = posicionX+radio*Math.cos(i*distancia)+'px';
-        divNuevo.style.width = '150px';
-        divNuevo.style.height = '80px';
-        divNuevo.style.border = '2px solid rgb(100, 34, 34)';
-        divLogo.appendChild(divNuevo);
-        divNuevo.appendChild(textoNuevo);
-    }
-}
-*/
