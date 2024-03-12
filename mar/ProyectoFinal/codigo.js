8ef08e2687e59ce3dcfc24089b6dd961130d25ca
marllanesgarcia <sakiro1999@gmail.com>
2024-03-12 20:14:09 +0100

    desarrollo del index.html y festividades.html
    cambios en la página principal añadiendo el nombre del usuario loggeado y creaccion de la página festividades.php

--- mar/ProyectoFinal/codigo.js
+++ mar/ProyectoFinal/codigo.js
@@ -1,40 +1,58 @@
 "use strict";
 
-var divLogo = document.getElementById('logo');
-var loginBtn = document.getElementById('login');
-var registroBtn = document.getElementById('registro');
-var volverBtn = document.getElementById('volver');
-
 document.addEventListener('DOMContentLoaded', function () {
+    var divLogo = document.getElementById('logo');
+    var loginBtn = document.getElementById('login');
+    var registroBtn = document.getElementById('registro');
+    var volverBtn = document.getElementById('volver');
+    var festividadesBtn = document.getElementById('festividades');
 
-   divLogo.addEventListener('click', function(){mostrarMenu()});
-    loginBtn.addEventListener('click', function(){
-        window.location.href = 'login.php';
-    });
-    registroBtn.addEventListener('click', function(){
-        window.location.href = 'registrarse.php';
-    });
-    volverBtn.addEventListener('click', function(){
-        window.location.href = 'index.php';
+    if (divLogo) {
+        divLogo.addEventListener('click', function () {
+            mostrarMenu();
+        });
+    }
+
+    if (loginBtn) {
+        loginBtn.addEventListener('click', function () {
+            window.location.href = 'login.php';
+        });
+    }
+
+    if (registroBtn) {
+        registroBtn.addEventListener('click', function () {
+            window.location.href = 'registrarse.php';
+        });
+    }
+
+    if (volverBtn) {
+        volverBtn.addEventListener('click', function () {
+            window.location.href = 'index.php';
+        });
+    }
+
+    festividadesBtn.addEventListener('click',function (){
+        window.location.href = 'festividades.php';
     });
 
-});
-
+})
 function mostrarMenu() {
     var divsPequeños = document.querySelector('.menu');
-    var estiloDisplay = window.getComputedStyle(divsPequeños).getPropertyValue('display');
+    if (divsPequeños) {
+        var estiloDisplay = window.getComputedStyle(divsPequeños).getPropertyValue('display');
 
-    if (estiloDisplay === 'none') {
-        divsPequeños.style.display = 'block';
-        setTimeout(function() {
-            divsPequeños.style.opacity = '10';
-            divsPequeños.style.transform = 'translateX(0)';
-        }, 80);
-    } else {
-        divsPequeños.style.opacity = '0';
-        divsPequeños.style.transform = 'translateX(-25px)';
-        setTimeout(function() {
-            divsPequeños.style.display = 'none';
-        }, 500);
+        if (estiloDisplay === 'none') {
+            divsPequeños.style.display = 'block';
+            setTimeout(function () {
+                divsPequeños.style.opacity = '10';
+                divsPequeños.style.transform = 'translateX(0)';
+            }, 80);
+        } else {
+            divsPequeños.style.opacity = '0';
+            divsPequeños.style.transform = 'translateX(-25px)';
+            setTimeout(function () {
+                divsPequeños.style.display = 'none';
+            }, 500);
+        }
     }
 }
---
+++ mar/ProyectoFinal/festividades.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MalagApp</title>
+    <link rel="stylesheet" href="./css/paginas.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Vidaloka&family=Viga&display=swap" rel="stylesheet">
+
+</head>
+
+<body class="festividades">
+
+    <div class="usuario">
+
+    </div>
+
+    <div class="titulo">
+
+        <div class="logo" id="logo">
+            <img src="logo.png" alt="logotipo MalaApp">
+        </div>
+
+        <h1>Festividades</h1>
+
+        <div id="juego">
+            <img src="./css/img/boton1.png" type="button" id="juego">
+        </div>
+    </div>
+
+    <div class="contenido">
+
+    </div>
+
+</body>
+
+</html>
